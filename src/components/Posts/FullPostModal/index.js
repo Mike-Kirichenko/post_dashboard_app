36b8c47a2f47db127e87350c6af95bed9fb56c5d
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-09-23 10:48:26 +0400

    style-changes-2

--- src/components/Posts/FullPostModal/index.js
+++ src/components/Posts/FullPostModal/index.js
@@ -1,31 +1,29 @@
-import { useContext } from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import ModalContext from '../../../context/ModalContext';
-import './fullPostModal.css';
+import { useContext } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import ModalContext from "../../../context/ModalContext";
+import "./fullPostModal.css";
 
 const PostModal = () => {
   const { setfullPostModal } = useContext(ModalContext);
 
   return (
-    <>
-      <Modal
-        open
-        onClose={() => setfullPostModal({})}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
-        <Box id='fullPostBox'>
-          <Typography id='modal-modal-title' variant='h6' component='h2'>
-            Text in a modal
-          </Typography>
-          <Typography id='modal-modal-description'>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </>
+    <Modal
+      open
+      onClose={() => setfullPostModal({})}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box id="fullPostBox">
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description">
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </Modal>
   );
 };
 
--- src/components/UserPanel/index.js
+++ src/components/UserPanel/index.js
@@ -1,44 +1,49 @@
-import { useState } from 'react';
-import { Box } from '@mui/system';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Button from '@mui/material/Button';
+import { useState } from "react";
+import { Box } from "@mui/system";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Button from "@mui/material/Button";
 
 const UserPanel = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+  const userBoxStyle = {
+    width: "100%",
+    position: "fixed",
+    zIndex: 5,
+    top: "0px",
+    right: "0px",
+    backgroundColor: "white",
+    textAlign: "right"
+  };
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        marginBottom: '2.5%',
-      }}
-    >
+    <Box sx={userBoxStyle}>
       <Button
-        id='basic-button'
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup='true'
-        aria-expanded={open ? 'true' : undefined}
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
       >
         <KeyboardArrowDownIcon />
         @mk2294
       </Button>
       <Menu
-        id='basic-menu'
+        id="basic-menu"
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          'aria-labelledby': 'basic-button',
+          "aria-labelledby": "basic-button"
         }}
       >
         <MenuItem onClick={handleClose}>Change password</MenuItem>
